dccb37ca5095624cfc5153f64961dfbeb78fbe1d
elisaoktvna <elisaoktaviana985@gmail.com>
2024-09-09 15:10:10 +0700

    add product

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 import KategoriRoute from "./routes/KategoriRoute.js";
+import ProductRoute from './routes/ProductRoute.js';
+import './associations.js'; 
 
 const app = express();
 
@@ -10,5 +12,6 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(KategoriRoute);
+app.use(ProductRoute);
 
 app.listen(5000, ()=> console.log('server up and running...'));
---
+++ backend/models/ProductModel.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Product = db.define('produk', {
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    description: DataTypes.TEXT,
+    stock: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    categoryId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'kategori', 
+            key: 'id'
+        }
+    }
+}, {
+    freezeTableName: true
+});
+
+export default Product;
