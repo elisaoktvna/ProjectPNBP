e129afdb249891ac9548f03f66f09ceb222b8338
elisaoktvna <elisaoktaviana985@gmail.com>
2024-09-10 14:02:26 +0700

    add user

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,43 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+// import db from "./config/Database.js";
 import KategoriRoute from "./routes/KategoriRoute.js";
 import ProductRoute from './routes/ProductRoute.js';
+import UserRoute from './routes/UserRoute.js';
 import './associations.js'; 
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// (async()=>{
+//     await db.sync();
+// })();
+
+// app.use(cors());
+//baru
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+//baru
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}));
+
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(KategoriRoute);
 app.use(ProductRoute);
+app.use(UserRoute);
 
 app.listen(5000, ()=> console.log('server up and running...'));
